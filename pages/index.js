c205e2be4881652ed9651f1aa3ef78786d3dc44f
bnwilson <bwilson.projects@gmail.com>
2019-12-19 18:48:27 -0500

    Added handling of admin redirect in index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import React, {useEffect} from 'react';
 import Main from './components/Main';
 import Head from 'next/head';
 
 const Home = () => {
+    useEffect(() => {
+        if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+            if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+                });
+            }
+        })
+    }})
+
     return (
         <div>
             <div className="app-wrapper">
