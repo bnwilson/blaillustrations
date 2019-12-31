c119d58a9c91c0f783323467e9677428024a54fb
bnwilson <bwilson.projects@gmail.com>
2019-12-31 00:37:30 -0500

    moving email send to function

--- pages/contact.js
+++ pages/contact.js
@@ -2,13 +2,30 @@ import React, {useRef} from 'react';
 import css from '../static/contact.css';
 import {useForm} from 'react-hook-form'
 
+// 12/30 -- Will add function in Netlify that will trigger an email
+//          via '@sendgrid/mail'
+//  ref: https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail
+
 
 export default function Contact () {
-    const BLA_EMAIL = 'YmxhaWxsdXN0cmF0aW9uc0BnbWFpbC5jb20='
-    const {register, handleSubmit} = useForm()
+    // const BLA_EMAIL = 'YmxhaWxsdXN0cmF0aW9uc0BnbWFpbC5jb20='
+    const {register, handleSubmit} = useForm({
+        defaultValues: {
+            firstName: "First",
+            lastName: "Last",
+        }
+    })
+
     const onSubmit = (data) => {
-        let emailStart = 'mailto:' + atob(BLA_EMAIL)
-        window.location.href()
+        console.log(data);
+        // const {firstName, lastName, subject, mailBody} = data;
+        // let emailCmdStart = 'mailto:' + atob(BLA_EMAIL);
+        // let subjetEncoded = `?subject=${encodeURI(
+        //     "Hello, I'm " + firstName + lastName + " and I have a " +  subject
+        // )}`;
+        // let bodyEncoded = `&body=${encodeURI(mailBody)}`;
+        // console.log("Email link",  emailCmdStart + subjetEncoded + bodyEncoded)
+        // window.location.href = emailCmdStart + subjetEncoded + bodyEncoded;
     }
 
     const contacts = [
@@ -36,10 +53,30 @@ export default function Contact () {
 
 
     return (
-        <div className={css.contacts}>
-            {contacts.map((item, index) => (
-                <a className={css.contactItem} key={index} href={item.url || "https://google.com"}>{item.title}</a>
-            ))}
+        <div className={css.wrapper}>
+            <form className={css.contact_form} onSubmit={handleSubmit(onSubmit)}>
+                <input className={css.contact_input} name="firstName" ref={register} />
+                <input className={css.contact_input} name="lastName" ref={register} />
+                <select className={css.contact_selection} name="subject" ref={register}>
+                    <option value="Commission Request">
+                        Commission Request
+                    </option>
+                    <option value="General Question">
+                        General Question
+                    </option>
+                    <option value="Special Order">
+                        Special Order
+                    </option>
+                </select>
+                <textarea className={css.contact_body} name="mailBody"></textarea>
+                <button className={css.contact_button}>sSubmit</button>
+            </form>
+
+            <div className={css.contacts}>
+                {contacts.map((item, index) => (
+                    <a className={css.contactItem} key={index} href={item.url || "https://google.com"}>{item.title}</a>
+                ))}
+            </div>
         </div>
     )
 }
