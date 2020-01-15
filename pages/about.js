1704e4f1b56e4b3b9ecbc54b92ca682bb6dda9dc
bnwilson <bwilson.projects@gmail.com>
2020-01-14 23:46:21 -0500

    Added minimal About content

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,19 @@
 import css from '../static/about.css';
 
+const charlieLogo = "/16bitCharlie.png";
+
 function About () {
     return (
-        <div className="main-about">
+        <div className={css.main_about}>
             <p className={css.about}>
-                It's me, Brittany!
+                Hi, I'm Brittany. I'm an artist from Columbus, Ohio.  
+                <br/>
+                I don't have much for this space, so here's my cat Charlie:
+                <br/><br/>
             </p>
+            <img className={css.about_charlie}
+                    src={charlieLogo}
+            />
         </div>
     )
 }
