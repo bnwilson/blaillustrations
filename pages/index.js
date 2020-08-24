7e70722e50957acb0d6765c6994e7025fbc39930
bnwilson <bwilson.projects@gmail.com>
2020-08-23 21:52:56 -0400

    Added link to shopify on nav and Main page

--- pages/index.js
+++ pages/index.js
@@ -21,21 +21,22 @@ const Home = () => {
         <div>
             <div className="app-wrapper">
                 <section className="home-wrapper">
+                    <h4 className="home-body">
+                        <h3 className="home-body__title">August 2020 - BLAIllustrations store is up!</h3> 
+                        <br/>
+                        Check out the <a href="https://blaillustrations.myshopify.com">
+                            BLAIllustrations store on Shopify
+                        </a>!
+
+                    </h4>
+                    <h4 className="home-body">
+                        The site is currently still a work in progress, 
+                        but feel free to check out the <Link href="/gallery"><a >Gallery</a></Link><br/>
+                        Or drop Brittany a line in the <Link href="/contact"><a >Contact Page</a></Link>!
+                    </h4>
                     <h2 className="home-title">
                         Thank you for visiting BLAIllustrations!
                     </h2>
-                    <br/><br/>
-                    <h4 className="home-body">
-                        The site is currently a work in progress, but feel free to check out the 
-                        <Link href="/gallery">
-                            <a > Gallery </a>
-                        </Link>
-                        or drop Brittany a line in the 
-                        <Link href="/contact">
-                            <a > Contact Page </a>
-                        </Link>
-                        !
-                    </h4>
                 </section>
             </div>
         </div>
